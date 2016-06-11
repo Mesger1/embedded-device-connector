00c7d8bb4963e1076cb5009d6aad30a2358efbb7
Mestdagh Gerd <mestdagh.gerd@gmail.com>
2016-06-11 12:24:58 +0000

    work on packages

--- build_deb_package.sh
+++ build_deb_package.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-VERSION = $1
+VERSION=$1
 rm -rf embedded-device-connector-*
-mkdir embedded-device-connector-${VERSION}
-mkdir embedded-device-connector-${VERSION}/DEBIAN
-cp control embedded-device-connector-${VERSION}/DEBIAN
-#cp preinst.sh ./embedded-device-connector-1.0/DEBIAN/preinst
+mkdir embedded-device-connector-$VERSION
+mkdir embedded-device-connector-$VERSION/DEBIAN
+cp control embedded-device-connector-$VERSION/DEBIAN
+cp preinst.sh ./embedded-device-connector-1.0/DEBIAN/preinst
 #cp postinst.sh ./embedded-device-connector-1.0/DEBIAN/postinst
-dpkg-deb --build embedded-device-connector-${VERSION}/
+dpkg-deb --build embedded-device-connector-$VERSION/
 
--- readme.md
+++ readme.md
@@ -1,11 +1,15 @@
 #WIFI ACCESS POINT CONFIGURATOR
 
+update sources.list for node 5.x apt-get :
+- curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
+
 Build debian package :
 - sudo ./build_deb_package.sh ${VERSION}
 
 Install debian package : 
 - sudo dpkg -i embedded-device-connector-${VERSION}.deb
 - sudo apt-get -f install
+
 
 
 
