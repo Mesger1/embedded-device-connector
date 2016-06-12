fa0ddfa7f78b149a02bfbc18e62fa87c2de856e6
Mestdagh Gerd <mestdagh.gerd@gmail.com>
2016-06-12 15:19:31 +0000

    refactor scripts

--- debian/preinst.sh
+++ debian/preinst.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 sudo  rm -rf /etc/dnsmasq.conf
-echo "/etc/resolv.conf"
-echo "removing file"
-sudo rm -rf /etc/resolv.conf
-echo "Creating File"
-cat > /etc/resolv.conf << EOF
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-EOF
+#echo "/etc/resolv.conf"
+#echo "removing file"
+#sudo rm -rf /etc/resolv.conf
+#echo "Creating File"
+#cat > /etc/resolv.conf << EOF
+#nameserver 8.8.8.8
+#nameserver 8.8.4.4
+#EOF
 #wget https://nodejs.org/dist/v4.3.2/node-v4.3.2-linux-armv6l.tar.gz 
 #tar -xvf node-v4.3.2-linux-armv6l.tar.gz > /dev/null 2>&1
 #cd node-v4.3.2-linux-armv6l
