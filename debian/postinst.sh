29034bd55226a4d723ebb466a891f059b354b179
Mestdagh Gerd <mestdagh.gerd@gmail.com>
2016-06-26 15:25:09 +0000

    colored logging

--- debian/postinst.sh
+++ debian/postinst.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-#echo "/etc/resolv.conf"
-#echo "removing file"
-#sudo rm -rf /etc/resolv.conf
-#echo "Creating File"
-#cat > /etc/resolv.conf << EOF
-#nameserver 8.8.8.8
-#nameserver 8.8.4.4
-#EOF
+echo "/etc/resolv.conf"
+echo "removing file"
+sudo rm -rf /etc/resolv.conf
+echo "Creating File"
+cat > /etc/resolv.conf << EOF
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
 
 if [ "$EUID" -ne 0 ]
         then echo "Must be root"
@@ -17,7 +17,8 @@ fi
 npm install https://github.com/gerdmestdagh/embedded-device-connector.git -g
 
 echo "adding server startup to rc.local"
-sed -i 's/wifi-connector \&//g' /etc/rc.local
-sed -i '$ i\wifi-connector \&' /etc/rc.local
-echo "PLEASE REBOOT NOW !!"
+sed -i '$ i\sudo wifi-connector' /etc/rc.local
+
+sudo wifi-connector
+
 exit 0  
--- debian/preinst.sh
+++ debian/preinst.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
-sudo  rm -rf /etc/dnsmasq.conf
-#echo "/etc/resolv.conf"
-#echo "removing file"
-#sudo rm -rf /etc/resolv.conf
-#echo "Creating File"
-#cat > /etc/resolv.conf << EOF
-#nameserver 8.8.8.8
-#nameserver 8.8.4.4
-#EOF
-#wget https://nodejs.org/dist/v4.3.2/node-v4.3.2-linux-armv6l.tar.gz 
-#tar -xvf node-v4.3.2-linux-armv6l.tar.gz > /dev/null 2>&1
-#cd node-v4.3.2-linux-armv6l
-#sudo cp -R * /usr/local/ > /dev/null 2>&1
-#cd ..
+echo "/etc/resolv.conf"
+echo "removing file"
+sudo rm -rf /etc/resolv.conf
+echo "Creating File"
+cat > /etc/resolv.conf << EOF
+nameserver 8.8.8.8
+nameserver 8.8.4.4
+EOF
+
+curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
