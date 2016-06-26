f3299ca940d06dec7bc7e2d7301d8ad69a13ece5
Mestdagh Gerd <mestdagh.gerd@gmail.com>
2016-06-26 17:51:19 +0000

    debian scripts

--- debian/preinst.sh
+++ debian/preinst.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 
 sed -i 's/dns-nameservers 8.8.8.8 8.8.4.4//g' /etc/network/interfaces
-sed -i '$ i\dns-nameservers 8.8.8.8 8.8.4.4' /etc/network/interfaces
+sed -i '${s/$/\ndns-nameservers 8.8.8.8 8.8.4.4/}' /etc/network/interfaces
 
 
