1fef5888a222e67748603a7edbfa6ab9827c4768
Mestdagh Gerd <mestdagh.gerd@gmail.com>
2016-06-26 17:30:09 +0000

    wpa_supplicant fix

--- debian/preinst.sh
+++ debian/preinst.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sed -i 's/dns-nameservers 8.8.8.8 8.8.4.4//g' /etc/rc.local
-sed -i '$ i\dns-nameservers 8.8.8.8 8.8.4.4' /etc/rc.local
+sed -i 's/dns-nameservers 8.8.8.8 8.8.4.4//g' /etc/network/interfaces
+sed -i '$ i\dns-nameservers 8.8.8.8 8.8.4.4' /etc/network/interfaces
 
 
