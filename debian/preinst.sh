02bca1db9b47ebebdcdcbb27062506b3f5dabdb9
Mestdagh Gerd <mestdagh.gerd@gmail.com>
2016-06-26 17:24:43 +0000

    dns fix

--- debian/preinst.sh
+++ debian/preinst.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo rm -rf /etc/resolv.conf
-cat > /etc/resolv.conf << EOF
-nameserver 8.8.8.8
-nameserver 8.8.4.4
-EOF
+
+sed -i 's/dns-nameservers 8.8.8.8 8.8.4.4//g' /etc/rc.local
+sed -i '$ i\dns-nameservers 8.8.8.8 8.8.4.4' /etc/rc.local
+
+
